70acedd8d40d2cb407999e7f29ba10c8cc11ca8a
volkantemelli <volkan.temelli@gmail.com>
2024-04-04 13:30:21 +0200

    project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleIncreaseStep() {
+    setStep((s) => s + 1);
+  }
+
+  function handleDecreaseStep() {
+    setStep((s) => s - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>Date Counter</h1>
+      <div className="step-div">
+        <button onClick={handleDecreaseStep}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={handleIncreaseStep}>+</button>
+      </div>
+      <div className="count-div">
+        <button
+          onClick={() => {
+            setCount((c) => c - step);
+          }}
+        >
+          -
+        </button>
+        <p>Count: {count}</p>
+        <button
+          onClick={(c) => {
+            setCount((c) => c + step);
+          }}
+        >
+          +
+        </button>
+      </div>
+      <p>
+        <span>
+          {count === 0 && `Today is `}
+          {count > 0 && `${count} days from today is `}
+          {count < 0 && `${count * -1} days ago was `}
+        </span>
+        <span className="date-span">{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
